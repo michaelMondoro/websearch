2ef271f3827117a8fcfaf3e31a4c5579b01d1194
michael <michaelmondoro@gmail.com>
2025-11-22 10:23:33 -0500

    clean up struture

--- test/SearchTest.py
+++ test/SearchTest.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/')))
 
 from Search import *
 
